9bbfad1cc557c3f85b28b3becc04f4ec91651f76
Tkay99 <44841004+Tkay99@users.noreply.github.com>
2019-01-02 13:05:29 -0600

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -34,7 +34,7 @@ namespace GradeBook.GradeBooks
         {
             if(Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 students");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
             }
 
@@ -45,7 +45,7 @@ namespace GradeBook.GradeBooks
         {
             if(Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 students");
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
                 return;
             }
 
